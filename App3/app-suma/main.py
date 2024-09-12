b2b650021ab003bedd84879de5e736ac4e888ced
alilm12345 <alilm061023@gmail.com>
2024-09-11 20:23:26 -0600

    app3

---
+++ App3/app-suma/main.py
@@ -0,0 +1,16 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = " suma de dos numeros"
+    page.bgcolor = "blue"
+    page.izquierda_alignment=ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    
+    lbl1=ft.Text("suma")
+                    Style=ft.TextStyle(size=50,weigh="bold")
+    ing1=ft.image(size)
+    
+    
+
+ft.app(main)
